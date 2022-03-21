bd5fefbfe8ff7fe8ea33f02f187c43f145aa286c
manaspalaparthi <manas.palaparthi@gmail.com>
2022-03-22 07:10:13 +1100

    Docker file update
    Docker file update , comments

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# service-template
- Datashop service template
+#Datashop service template
+
+
+**Building a docker**
+
+Include necessary packages for your model in requirements.txt. We prefer you to use pip freeze > requirements.txt 
+
+
+**RUN Command to build DOCKER image**
+
+To run the container 
+
+```
+docker build -d -t servicename .
+```
+
+**RUN Command to RUN DOCKER container**
+
+```
+docker run -d -p 5000:5000 servicename
+```
--- app.py
+++ app.py
@@ -22,24 +22,21 @@ class predict(Resource):
             # notify the Datashop application that job is in "Running" state
             Datashop.updateJob(jobID,"running", None)
 
-            # download userinput data and save in "tmp" folder
+            # Phase 1:  download userinput data and save in "tmp" folder
             Datashop.get_data(jobID, inputdata)
 
             """
             call the service below  
             follow service documentation for service output format
             """
-
+            # Phase 2:  call the service
             service_results = service.run(jobID) # returns list of all the results
 
-            # save results
+            # Phase 3:  save the results and update job status
             insightsS3Link = Datashop.save_results("filename", service_results[0], datatype="int")  #specify the dtype of result (image,csv,graph,json,str,int)
-
-            # calculate job duration
             duration = time.time() - start
-
-            # insightsS3Link can be a list of links [insightsS3Link1 ,insightsS3Link2 , insightsS3Link3]
-            insights_payload = Datashop.updateJob(jobID,insightsS3Link,duration)
+            #Update job status
+            insights_payload = Datashop.updateJob(jobID,insightsS3Link,duration) #insightsS3Link can be a list of links [insightsS3Link1 ,insightsS3Link2 , insightsS3Link3]
 
             return {"result": "success","duration":duration,"insightFileURL":insights_payload}
                         
@@ -59,6 +56,6 @@ class predict(Resource):
 api.add_resource(predict,'/predict')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
